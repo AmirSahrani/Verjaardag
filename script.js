247400e2d909f5cdfa77b0ae231782439e629251
Amir Sahrani <amirsahrani1@gmail.com>
2024-11-30 15:18:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function claimPrize() {
+  // create popup that says "Congratulations! You have won a prize!"
+  alert("U heeft een virus gewonnen!");
+}
+
+function replaceText() {
+  // replace all text on the page with "You have been hacked!"
+  document.body.innerHTML = "<h1>Al je data is van ons!</h1>";
+}
